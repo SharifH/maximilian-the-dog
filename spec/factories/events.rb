10bf9aae3097d72ba95a176500d1d669b941747b
Donielle Berg <donielle.berg@gmail.com>
2013-08-04 23:43:35 -0700

    Add rspec tests

--- spec/factories/events.rb
+++ spec/factories/events.rb
@@ -3,6 +3,6 @@
 FactoryGirl.define do
   factory :event do
     title 'KIPP School Summit'
-    description 'KIPP School Summit is comprised of four days of professional development, relationship building, and celebration that kicks off at the New to KIPP brunch at 10:30 AM on Monday and concludes at the closing Gala Awards & Celebration Thursday evening.'
+    description 'KIPP School Summit is comprised of four days of professional development, relationship building, and celebration that kicks off at the New to KIPP brunch at 10:30AM on Monday and concludes at the closing Gala Awards & Celebration Thursday evening.'
   end
 end
