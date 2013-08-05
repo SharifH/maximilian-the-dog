b516f1dd46a74c7086d26be8a1e45045928cb952
staxworks <matthewp.owens@gmail.com>
2013-08-04 20:29:09 -0700

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MaximilianTheDog::Application.config.secret_token = '92f595605d62ca50be0c0a3a296ab93eaa37815d7f7201b650c2316957f53aef616b5d046d0bfb3ad807f67ae06f06ef0bfb68e42c51710596872f5e0b5569ff'
